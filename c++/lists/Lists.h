9a763c6b24f13f0a523928eea18d9cb09daa7a0a
Zoidberg <zoidberg@placave-pro15.local>
2017-02-04 00:32:25 +0000

    style c++

--- c++/lists/Lists.h
+++ c++/lists/Lists.h
@@ -5,23 +5,23 @@
 #include <stdexcept>
 
 namespace Lists {
-    template<typename T>
+  template<typename T>
     T returnLast(const std::list<T> _list) {
-        if (_list.empty()) {
-            throw new std::invalid_argument("List is empty");
-        }
-        return _list.back();
+    if (_list.empty()) {
+      throw new std::invalid_argument("List is empty");
     }
-
-    template<typename T>
+    return _list.back();
+  }
+  
+  template<typename T>
     std::list<T> returnLastAndPenultimate(const std::list<T> _list) {
-        if (_list.size() < 2) {
-            throw new std::invalid_argument("List has less than 2 elements");
-        }
-
-        auto last_it = std::prev(_list.end());
-        return {*std::prev(last_it), *last_it};
+    if (_list.size() < 2) {
+      throw new std::invalid_argument("List has less than 2 elements");
     }
+    
+    auto last_it = std::prev(_list.end());
+    return {*std::prev(last_it), *last_it};
+  }
 }
 
 
--- c++/test/TestLists.cpp
+++ c++/test/TestLists.cpp
@@ -2,23 +2,23 @@
 #include <Lists.h>
 
 TEST_CASE( "Should return last int of an int list", "[lastOfIntList]" ) {
-    std::list<int> dummy = {1, 2, 3, 4, 5};
-    REQUIRE(Lists::returnLast<int>(dummy) == 5);
+  std::list<int> dummy = {1, 2, 3, 4, 5};
+  REQUIRE(Lists::returnLast<int>(dummy) == 5);
 }
 
 TEST_CASE( "Should return last char of a char list", "[lastOfCharList]" ) {
-    std::list<char> dummy = {'a', 'b', 'c', 'd'};
-    REQUIRE(Lists::returnLast<char>(dummy) == 'd');
+  std::list<char> dummy = {'a', 'b', 'c', 'd'};
+  REQUIRE(Lists::returnLast<char>(dummy) == 'd');
 }
 
 TEST_CASE( "Should return last and penultimate int of an int list", "[lastAndPenultimateOfIntList]" ) {
-    std::list<int> dummy = {1, 2, 3, 4, 5};
-    std::list<int> res = {4, 5};
-    REQUIRE(Lists::returnLastAndPenultimate<int>(dummy) == res);
+  std::list<int> dummy = {1, 2, 3, 4, 5};
+  std::list<int> res = {4, 5};
+  REQUIRE(Lists::returnLastAndPenultimate<int>(dummy) == res);
 }
 
 TEST_CASE( "Should return last and penultimate char of a char list", "[lastAndPenultimateOfCharList]" ) {
-    std::list<char> dummy = {'a', 'b', 'c', 'd'};
-    std::list<char> res = {'c', 'd'};
-    REQUIRE(Lists::returnLastAndPenultimate<char>(dummy) == res);
+  std::list<char> dummy = {'a', 'b', 'c', 'd'};
+  std::list<char> res = {'c', 'd'};
+  REQUIRE(Lists::returnLastAndPenultimate<char>(dummy) == res);
 }
